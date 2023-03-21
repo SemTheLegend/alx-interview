f5b85ba84e668a1fedcd599ca6f199eb313885a9
SemTheLegend <semmathe@gmail.com>
2023-03-21 10:51:49 -0700

    pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,16 +3,19 @@
 def pascal_triangle(n):
     """pascals triangle, returns an empty list if n <= 0."""
 
+    res = []
+
     if n <= 0:
-        return []
+        return res
 
-    res = [[1]]
+    for i in range(n):
+        temp_list = []
 
-    for i in range(n - 1):
-        temp = [0] + res[-1] + [0]
-        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(res[i-1][j-1] + res[i-1][j])
+        res.append(temp_list)
 
-        for j in range(len(res[-1]) + 1):
-            row.append(temp[j] + temp[j + 1])
-        res.append(row)
     return res
